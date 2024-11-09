9fc6cd866d2120c9a0a7b4e66598060f14dc8eee
Charles <c4a@monocles.eu>
2024-11-09 17:00:01 -0500

    Many changes
    - Icon
    - Back button
    - User agent
    - Loading animation fix

--- app/src/main/java/to/us/charlesst/discord/MainActivity.kt
+++ app/src/main/java/to/us/charlesst/discord/MainActivity.kt
@@ -18,8 +18,14 @@ class MainActivity : AppCompatActivity() {
                 val webSettings: WebSettings = webView.getSettings()
                 webView.settings.javaScriptEnabled = true // for obvious reasons, Discord is a JS-heavy app
                 webView.webViewClient = WebViewClient()
-                webView.settings.userAgentString = "Android" // prevent fingerprinting by user-agent
+                webView.settings.userAgentString = "Android (+https://github.com/charles8191/discord)" // prevent fingerprinting by user-agent
                 webSettings.setDomStorageEnabled(true) // used for tokens
+                webSettings.mediaPlaybackRequiresUserGesture = false; // fix loading animation
                 webView.loadUrl("https://discord.com/app")
+    }
+
+    override fun onBackPressed() { // fix back button
+        val webView: WebView = findViewById(R.id.webview)
+        webView.goBack()
     }
 }
